c8c69c3aeacea22eb57c3d6e861c0d662cce785a
yaa-zalmaty <jclarksummit@gmail.com>
2022-12-16 17:27:45 -0500

    Added JS script for game and updated UI

--- rps-script.js
+++ rps-script.js
@@ -11,28 +11,31 @@ const rockButton = document.getElementById("rockChoice");
 rockButton.addEventListener('click', function() {
     playerChoice = 'rock';
     computerChoice = getComputerChoice();
-    console.log(playerChoice);
-    console.log(computerChoice);
+    playRound(playerChoice, computerChoice);
 });
 
 const scissorsButton = document.getElementById("scissorsChoice");
 scissorsButton.addEventListener('click', function() {
     playerChoice = 'scissors';
     computerChoice = getComputerChoice();
-    console.log(playerChoice);
-    console.log(computerChoice);
+    playRound(playerChoice, computerChoice);
 });
 
 const paperButton = document.getElementById('paperChoice');
 paperButton.addEventListener('click', function() {
     playerChoice = 'paper';
     computerChoice = getComputerChoice();
-    console.log(playerChoice);
-    console.log(computerChoice);
+    playRound(playerChoice, computerChoice);
 })
 
-// Generate computer choice
+// Update scoreboard
+const computerScoreboard = document.getElementById('computerScoreboard');
+computerScoreboard.textContent = `Computer: ${computerScore}`;
 
+const playerScoreboard = document.getElementById('playerScoreboard');
+playerScoreboard.textContent = `Player: ${playerScore}`;
+
+// Generate computer choice
 function getComputerChoice (){
     switch (Math.floor(Math.random() * 3)){
         case 0: return 'rock';
@@ -41,3 +44,20 @@ function getComputerChoice (){
     }
 }
 
+// Play a round of RPS
+
+function playRound(playerChoice, computerChoice){
+    if (playerChoice === computerChoice) {
+        console.log("tie");
+    } else if (playerChoice === 'rock' && computerChoice === 'scissors' ||
+        playerChoice === 'paper' && computerChoice === 'rock' ||
+        playerChoice === 'scissors' && computerChoice === 'paper'){
+        console.log("You win");
+        playerScore++;
+    } else {
+        console.log("You lose");
+        computerScore++;
+    }
+    playerScoreboard.textContent = `Player: ${playerScore}`;
+    computerScoreboard.textContent = `Computer: ${computerScore}`;
+}
